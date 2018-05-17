cb59750bd55205b0d0710d1f3995aca0cf763c98
«NikitaDurasov» <yassnda@gmail.com>
2018-05-17 16:46:39 +0300

    add data tools

--- SPD.py
+++ SPD.py
@@ -1,32 +1,34 @@
-import torch 
+import torch
 from torch import nn
 from torch import FloatTensor
 from torch.autograd import Variable
 from torch import optim
 from torch.nn.modules.loss import MSELoss
 
+from tqdm import tqdm
+
 from numpy.linalg import matrix_rank
 
 from sklearn.datasets import make_spd_matrix
 
 # TO DO
-# test function on larger datasets 
-# try to restore graph from its laplacian matrix 
+# test function on larger datasets
+# try to restore graph from its laplacian matrix
 # write comments
 
 class Linear2D(nn.Module):
-    
+
     def __init__(self, in_features, out_features):
         super(Linear2D, self).__init__()
         self.W = nn.Parameter(torch.rand((out_features, in_features)))
-        
+
     def forward(self, X):
         XW = self._matmul(X, self.W.transpose(0 ,1))
         WXW = self._matmul(XW, self.W, kind='left')
         return WXW
-        
+
     def _matmul(self, X, Y, kind='right'):
-        results = [] 
+        results = []
         for i in range(X.size(0)):
             if kind == 'right':
                 result = torch.mm(X[i], Y)
@@ -34,7 +36,7 @@ class Linear2D(nn.Module):
                 result = torch.mm(Y, X[i])
             results.append(result.unsqueeze(0))
         return torch.cat(results, 0)
-    
+
     def check_rank(self):
         return min(self.W.data.size()) == matrix_rank(self.W.data.numpy())
 
@@ -43,16 +45,16 @@ class SymmetricallyCleanLayer(nn.Module):
     def __init__(self):
         super(SymmetricallyCleanLayer, self).__init__()
         self.relu = nn.ReLU()
-        
+
     def forward(self, X):
-        return self.relu(X) 
+        return self.relu(X)
 
 
 class NonLinearityBlock(nn.Module):
     def __init__(self, activation):
         super(NonLinearityBlock, self).__init__()
         self.activation = activation
-        
+
     def forward(self, X):
         return self.activation(X)
 
@@ -63,7 +65,7 @@ class ShrinkBlock(nn.Module):
         self.weights_matrix = nn.Parameter(torch.zeros((features, features)).normal_())
         self.weights_matrix_square = torch.mm(self.weights_matrix.transpose(0, 1), self.weights_matrix)
         self.inverse_eye = Variable(torch.ones((features, features)) - torch.eye(features))
-    
+
     def forward(self, X):
         return X - self.weights_matrix_square * self.inverse_eye
 
@@ -72,7 +74,7 @@ class MatrixEncoder(nn.Module):
     def __init__(self, n_features):
         super(MatrixEncoder, self).__init__()
         self.n_features = n_features
-        
+
         self.encoder = nn.Sequential(
             Linear2D(n_features, 5),
             ShrinkBlock(5),
@@ -80,7 +82,7 @@ class MatrixEncoder(nn.Module):
             Linear2D(5, 3),
             ShrinkBlock(3)
         )
-        
+
         self.decoder = nn.Sequential(
             Linear2D(3, 5),
             ShrinkBlock(5),
@@ -88,22 +90,31 @@ class MatrixEncoder(nn.Module):
             Linear2D(5, n_features),
             ShrinkBlock(n_features)
         )
-        
+
     def forward(self, X):
         return self.decoder(self.encoder(X))
 
 
 def run_test():
 	coder = MatrixEncoder(6)
-	matrix = Variable(FloatTensor(make_spd_matrix(6))).unsqueeze(0)
-	optimizer = optim.Adam(coder.parameters(), lr=0.01)
+        dataset = [Variable(FloatTensor(make_spd_matrix(6)).unsqueeze(0)) for _ in range(10)]
+        dataset = torch.cat(dataset, 0)
+	# matrix = Variable(FloatTensor(make_spd_matrix(6))).unsqueeze(0)
+	optimizer = optim.Adam(coder.parameters(), lr=0.1)
 	criterion = MSELoss()
 
-	for epoch in range(4000):
-	    optimizer.zero_grad()
-	    outputs = coder(matrix)
-	    loss = criterion(outputs, matrix)
-	    loss.backward(retain_graph=True)
-	    if epoch % 100 == 0:
-	        print(loss.data[0])
-	    optimizer.step()
+	for epoch in tqdm(range(1000)):
+            optimizer.zero_grad()
+            outputs = coder(dataset)
+            loss = criterion(outputs, dataset)
+            loss.backward(retain_graph=True)
+            #if epoch % 100 == 0:
+            #    print("EPOCH: {0}, LOSS: {1}".format(epoch, loss.data[0]))
+            optimizer.step()
+
+        print(coder(dataset[0].unsqueeze(0)))
+        print(dataset[0].unsqueeze(0))
+
+
+if __name__ == "__main__":
+    run_test()
---
+++ dataset_tools.py
@@ -0,0 +1,15 @@
+import networkx as nx 
+import os 
+import numpy as np
+
+
+def read_test_graphs():
+	graphs = []
+	for graph in os.listdir('./test_graphs/')[1:]:
+		graphs.append(nx.read_graphml('./test_graphs/' + graph))
+    return graphs
+
+def build_dataset(graphs, nodes_number=6):
+	for graph in graphs:
+	    graph.remove_nodes_from(list(graph.nodes)[nodes_number:])
+    return graphs
